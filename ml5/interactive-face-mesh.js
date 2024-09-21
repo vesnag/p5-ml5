aec834648708ffc245ac0f316c2c0b38269a1fb3
Vesna Groselj <vgroselj@gmail.com>
2024-09-21 22:52:28 +0200

    Add sound.

--- ml5/interactive-face-mesh.js
+++ ml5/interactive-face-mesh.js
@@ -13,39 +13,69 @@ const COLORS = {
   darkRed: '#98254c',
 };
 
-const options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
-let faceMesh;
-let video;
-let faces = [];
+const faceMeshOptions = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
+let faceMesh, video, faces = [];
+let mic, fft;
+let bgColor = COLORS.white;
+const colorKeys = Object.keys(COLORS);
 
 function preload() {
-  faceMesh = ml5.faceMesh(options);
+  faceMesh = ml5.faceMesh(faceMeshOptions);
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(width, height);
   video.hide();
   faceMesh.detectStart(video, gotFaces);
-}
-
-function draw() {
-  image(video, 0, 0, width, height);
-
-  noStroke();
-
-  faces.forEach(face => {
-    face.keypoints.forEach((keypoint, index) => {
-      // Select a color from COLORS based on the keypoint index
-      const colorKeys = Object.keys(COLORS);
-      const color = COLORS[colorKeys[index % colorKeys.length]];
-      fill(color);
-      circle(keypoint.x, keypoint.y, 5);
-    });
-  });
+  mic = new p5.AudioIn();
+  mic.start();
+  fft = new p5.FFT(0.8, 64);
+  fft.setInput(mic);
 }
 
 function gotFaces(results) {
   faces = results;
+  faceMesh.detectStart(video, gotFaces);
+}
+
+function draw() {
+  const spectrum = fft.analyze();
+  const amplitude = fft.getEnergy("bass");
+  bgColor = getBackgroundColor(amplitude);
+  background(bgColor);
+  image(video, 0, 0, width, height);
+  noStroke();
+  faces.forEach(face => {
+    face.keypoints.forEach((keypoint, index) => {
+      fill(COLORS[colorKeys[index % colorKeys.length]]);
+      circle(keypoint.x, keypoint.y, 5);
+    });
+  });
+  drawFrequencyBars(spectrum);
+}
+
+function getBackgroundColor(amplitude) {
+  if (amplitude > 200) return COLORS.deepLavender;
+  if (amplitude > 150) return COLORS.brightMagenta;
+  if (amplitude > 100) return COLORS.lightPink;
+  if (amplitude > 50) return COLORS.softPink;
+  return COLORS.white;
+}
+
+function drawFrequencyBars(spectrum) {
+  noStroke();
+  const numBars = spectrum.length;
+  const barWidth = width / numBars;
+  for (let i = 0; i < numBars; i++) {
+    const amplitude = spectrum[i];
+    const y = map(amplitude, 0, 255, height, 0);
+    fill(COLORS[colorKeys[i % colorKeys.length]]);
+    rect(i * barWidth, y, barWidth - 2, height - y);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
